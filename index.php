5c83d878a14f044d083ea78f24d744467015fb7a
Fumetsu <magomed.khadaev@outlook.com>
2020-02-27 12:29:31 +0100

    add connection to database

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
-echo "Hello MoiJV";
+require_once "dbconfig.php";
+
+// new PDO($dsn, $dbuser, $dbpassword);
+// $dsn = 'mysql:host=...;dbname=...';
+
+$pdo = new PDO("mysql:host={$dbconfig['dbhost']};dbname={$dbconfig['dbname']}", $dbconfig['dbuser'], $dbconfig['dbpassword']);
 
 
+$jvs = $pdo->query("SELECT * FROM jv")->fetchAll();
